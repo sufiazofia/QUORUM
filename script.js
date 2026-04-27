0e99b58ca2d295d3f56afcafed92a392d0b312d3
sufiazofia <sofiacduarte1@gmail.com>
2026-04-27 19:00:49 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,151 +1,42 @@
-// Size of cells
-let cellSize = 5;
-
-// Probability of alive at start
-let probabilityOfAliveAtStart = 15;
-
-// Timer
-let interval = 100;
-let lastRecordedTime = 0;
-
-// Colors
-let alive, dead;
-
-// Grid
 let cells;
-let cellsBuffer;
+let cellSize = 10;
+let cols, rows;
 
-// Pause
-let pause = false;
+let textPoints = [];
+let textString = "QUORUM";
 
 function setup() {
-  createCanvas(640, 360);
+  createCanvas(window.innerWidth, window.innerHeight);
 
-  alive = color(235, 245, 238);
-  dead = color(120, 170, 140);
-
-
-  let cols = floor(width / cellSize);
-  let rows = floor(height / cellSize);
+  // GRID
+  cols = floor(width / cellSize);
+  rows = floor(height / cellSize);
 
   cells = new Array(cols).fill().map(() => new Array(rows).fill(0));
-  cellsBuffer = new Array(cols).fill().map(() => new Array(rows).fill(0));
 
-  stroke(173, 216, 230);
-  noSmooth();
-
-  // Initialize cells
   for (let x = 0; x < cols; x++) {
     for (let y = 0; y < rows; y++) {
-      let state = random(100);
-      cells[x][y] = state > probabilityOfAliveAtStart ? 0 : 1;
+      cells[x][y] = random() > 0.85 ? 1 : 0;
     }
   }
 
-  background(0);
+  // TEXTO
+  textFont('Playfair Display');
+  textSize(min(width * 0.15, 180));
+
+  let bounds = textBounds(textString, 0, 0);
+
+  let x = (width - bounds.w) / 2;
+  let y = (height + bounds.h) / 2;
+
+  textPoints = textToPoints(textString, x, y, textSize(), {
+    sampleFactor: 0.12
+  });
 }
 
 function draw() {
-  let cols = floor(width / cellSize);
-  let rows = floor(height / cellSize);
+  background(235, 245, 238);
 
-  // Draw grid
-  for (let x = 0; x < cols; x++) {
-    for (let y = 0; y < rows; y++) {
-      fill(cells[x][y] === 1 ? alive : dead);
-      rect(x * cellSize, y * cellSize, cellSize, cellSize);
-    }
-  }
-
-  // Update
-  if (millis() - lastRecordedTime > interval) {
-    if (!pause) {
-      iteration();
-      lastRecordedTime = millis();
-    }
-  }
-
-  // Mouse interaction
-  if (pause && mouseIsPressed) {
-    let xCell = constrain(floor(mouseX / cellSize), 0, cols - 1);
-    let yCell = constrain(floor(mouseY / cellSize), 0, rows - 1);
-
-    cells[xCell][yCell] = cellsBuffer[xCell][yCell] === 1 ? 0 : 1;
-  } else if (pause && !mouseIsPressed) {
-    // Save buffer
-    for (let x = 0; x < cols; x++) {
-      for (let y = 0; y < rows; y++) {
-        cellsBuffer[x][y] = cells[x][y];
-      }
-    }
-  }
-}
-
-function iteration() {
-  let cols = floor(width / cellSize);
-  let rows = floor(height / cellSize);
-
-  // Copy to buffer
-  for (let x = 0; x < cols; x++) {
-    for (let y = 0; y < rows; y++) {
-      cellsBuffer[x][y] = cells[x][y];
-    }
-  }
-
-  for (let x = 0; x < cols; x++) {
-    for (let y = 0; y < rows; y++) {
-
-      let neighbours = 0;
-
-      for (let xx = x - 1; xx <= x + 1; xx++) {
-        for (let yy = y - 1; yy <= y + 1; yy++) {
-
-          if (xx >= 0 && xx < cols && yy >= 0 && yy < rows) {
-            if (!(xx === x && yy === y)) {
-              if (cellsBuffer[xx][yy] === 1) {
-                neighbours++;
-              }
-            }
-          }
-
-        }
-      }
-
-      if (cellsBuffer[x][y] === 1) {
-        if (neighbours < 2 || neighbours > 3) {
-          cells[x][y] = 0;
-        }
-      } else {
-        if (neighbours === 3) {
-          cells[x][y] = 1;
-        }
-      }
-    }
-  }
-}
-
-function keyPressed() {
-  let cols = floor(width / cellSize);
-  let rows = floor(height / cellSize);
-
-  if (key === 'r' || key === 'R') {
-    for (let x = 0; x < cols; x++) {
-      for (let y = 0; y < rows; y++) {
-        let state = random(100);
-        cells[x][y] = state > probabilityOfAliveAtStart ? 0 : 1;
-      }
-    }
-  }
-
-  if (key === ' ') {
-    pause = !pause;
-  }
-
-  if (key === 'c' || key === 'C') {
-    for (let x = 0; x < cols; x++) {
-      for (let y = 0; y < rows; y++) {
-        cells[x][y] = 0;
-      }
-    }
-  }
+  drawBackground();
+  drawText();
 }
